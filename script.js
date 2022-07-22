d064777d3f03fd4a38e5deef0944e0c3e4dbbcfd
Ab1433 <61510785+Ab1433@users.noreply.github.com>
2022-07-22 18:16:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,16 +16,17 @@ function compute()
     resultDisplay.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>"  + ", <br> at an interest rate of "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
 }
 
+
 function SliderValue()
 {
     var slider = document.getElementById("rate");
     var output = document.getElementById("rate_display");
     output.innerHTML = slider.value; // Display the default slider value
-
+    document.getElementById("rate").style.color="red";
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function() 
     {
         
         output.innerHTML = this.value;
-    }  
+    } 
 }
